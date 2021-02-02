69c6929878014205331eca67547890529647bd55
Adithya Shankaran <72570811+Adicr7fan@users.noreply.github.com>
2021-02-02 16:28:39 +0530

    Update question3.c

--- lecture-3/C/question3.c
+++ lecture-3/C/question3.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
-	int a=93,b=94,c=95,d=96,e=97;
-	printf("The ASCII character of 93 = %c",a);
+	int a=92,b=94,c=95,d=96,e=97;
+	printf("The ASCII character of 92 = %c",a);
 	printf("\n The ASCII character of 94 = %c",b);
 	printf("\n The ASCII character of 95 = %c",c);
 	printf("\n The ASCII character of 96 = %c",d);
